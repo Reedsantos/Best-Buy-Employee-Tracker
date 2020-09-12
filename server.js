79dde5b028e76445c24e72750e5d81124c14b502
Reed Santos <reedsantos@icloud.com>
2020-09-12 17:43:52 -0400

    add prompts

--- server.js
+++ server.js
@@ -17,5 +17,38 @@ connection.connect(function (err) {
         return;
     }
     console.log("connected as id " + connection.threadId);
+    startApp()
 });
 
+function startApp() {
+    clear();
+    startPrompt();
+}
+
+function renderScreen(tableTitle, tableData) {
+    clear();
+    console.log((tableTitle));
+    console.table(tableData);
+    startPrompt();
+}
+
+function startPrompt() {
+    console.log("\n Welcome To The Best Buy Retail Employee Tracker! \n");
+    inquirer
+        .prompt({
+            type: "list",
+            name: "promptChoice",
+            message: "Make a selection:",
+            choices: [
+                "View Employees",
+                "Sort Employees by Manager",
+                "Show Roles",
+                "Show Departments",
+                "Add Employee",
+                "Add Role",
+                "Add Departments",
+                "Remove Employee",
+                "Remove Role",
+                "Remove Department"]
+        })
+};
